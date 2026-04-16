0aff24c11d2268617971f591b10c86ce2fad26be
Jiahao Su <damow890@gmail.com>
2026-04-16 19:21:56 +0800

    update

--- script/onnx_docker_build.sh
+++ script/onnx_docker_build.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 set -e
 
-IMAGE_REPO="swr.cn-south-1.myhuaweicloud.com/onnx-repo/manylinux_2_28_x86_64"
-IMAGE_TAG="${1:-$(git rev-parse --short HEAD)}"
-NO_PUSH="${2:-}"
+IMAGE_REPO="swr.cn-southwest-2.myhuaweicloud.com/onnxruntime_image/manylinux_2_28"
+ARCH="aarch64"
+IMAGE_TAG=""
+NO_PUSH=false
 
-if [ "${NO_PUSH}" = "--no-push" ]; then
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --arch)    ARCH="$2";      shift 2 ;;
+    --tag)     IMAGE_TAG="$2"; shift 2 ;;
+    --no-push) NO_PUSH=true;   shift   ;;
+    *) echo "Unknown option: $1"; exit 1 ;;
+  esac
+done
+
+IMAGE_TAG="${IMAGE_TAG:-$(git rev-parse --short HEAD)}"
+
+if [ "${NO_PUSH}" = true ]; then
   OUTPUT="type=docker"
 else
   OUTPUT="type=image,push=true,oci-mediatypes=false"
 fi
-
 docker buildx build \
-  --build-arg ARCH=x86_64 \
-  -t "${IMAGE_REPO}:${IMAGE_TAG}" \
+  --build-arg ARCH=${ARCH} \
+  -t "${IMAGE_REPO}:${ARCH}-${IMAGE_TAG}" \
   -t "${IMAGE_REPO}:latest" \
   -f docker/onnxruntime.Dockerfile \
   --output "${OUTPUT}" .
